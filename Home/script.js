ad1331dbf18e1d49a020e27ddbc28d76e04d8a62
Ujjwal Kumar <ujjwalkumar949494@gmail.com>
2023-04-02 01:00:06 +0530

    Add files via upload

---
+++ Home/script.js
@@ -0,0 +1,30 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+const toggleNavbar = () => {
+ nav_header.classList.toggle("active");
+};
+mobile_nav.addEventListener("click",() => toggleNavbar());
+
+var slideIndex = 1;
+      showSlides(slideIndex);
+      function plusSlides(n) {
+         showSlides(slideIndex += n);
+      }
+      function currentSlide(n) {
+         showSlides(slideIndex = n);
+      }
+      function showSlides(n) {
+         var i;
+         var slides = document.getElementsByClassName("mySlides");
+         var dots = document.getElementsByClassName("dot");
+         if (n > slides.length) {slideIndex = 1}
+         if (n < 1) {slideIndex = slides.length}
+         for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+         }
+         for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+         }
+         slides[slideIndex-1].style.display = "block";
+         dots[slideIndex-1].className += " active";
+      }
